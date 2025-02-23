43faf39e56aaafdc61475d95e495037813e8ba84
Johann Heuer <johann.f.heuer@gmail.com>
2025-02-24 00:11:34 +0100

    look into parsing a request with bun

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,34 @@
 import { env } from "./lib/env";
+import { STATUS_BAD_REQUEST, statusText } from "./lib/http";
 
 console.log(`INFO Environment: ${env.ENVIRONMENT}`)
 
 const server = Bun.serve({
     routes: {
-        "/add_moment": req => {
-            return Response.json({ message: "Creating moment..." });
+        "/add_moment": {
+            POST: async (request, server) => {
+                let data: {
+                    token: string;
+                } | undefined = undefined;
+                try {
+                    let data = await request.json();
+                    console.log(data);
+                } catch (e) {
+                    console.error(e);
+                    return Response.json({
+                        success: false,
+                        message: "Could not parse request data."
+                    }, {
+                        status: STATUS_BAD_REQUEST,
+                        statusText: statusText(STATUS_BAD_REQUEST),
+                    })
+                }
+
+                return Response.json({
+                    success: true,
+                    message: "Creating moment...",
+                });
+            }
         },
     },
     port: env.PORT,
