8795420b92746b9671c5aaa1321dc4337a3f624b
Johann Heuer <johann.f.heuer@gmail.com>
2025-02-23 22:42:48 +0100

    initialize http server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("Hello via Bun!");
+import { Elysia } from "elysia";
+
+const PORT = 3000;
+
+const app = new Elysia()
+    .get("/", () => "Hello Elysia")
+    .listen(3000);
+
+console.log(
+    `Server is running at ${app.server?.hostname}:${app.server?.port}`
+);
