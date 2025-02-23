0e0560386b5d45134ba9882131da1c5b0d48a422
Johann Heuer <johann.f.heuer@gmail.com>
2025-02-23 23:51:51 +0100

    remove elysia :3

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,12 @@
-import { Elysia } from "elysia";
 import { env } from "./lib/env";
 
 console.log(`INFO Environment: ${env.ENVIRONMENT}`)
 
-const app = new Elysia()
-    .post("/add_moment", () => {
-        console.log("Adding moment...")
-        return;
-    })
-    .listen(env.PORT);
-
-console.log(
-    `INFO Server is running at ${app.server?.hostname}:${app.server?.port}`
-);
+const server = Bun.serve({
+    routes: {
+        "/add_moment": req => {
+            return Response.json({ message: "Creating moment..." });
+        },
+    },
+    port: env.PORT,
+})
